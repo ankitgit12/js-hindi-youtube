f8aa01e35e1cca3b03999024e117421de74a39db
ankitgit12 <144496631+ankitgit12@users.noreply.github.com>
2023-09-25 20:27:35 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+///    primitive 
+
+// 7 types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id ===anotherID);
+
+//const bigNumber = 4215523668563487n
+
+///   reference (non primitive)
+
+///3 types : Array , Object , Functions
+
+const heros = ["Shaktiman", "Naagraj" , "Dogo"]
+let myObj = {
+    name: "ankit",
+    age: 22,
+}
+
+const myFunction = function () {
+      console.log("hello world");
+    
+}
+
+console.log(typeof myFunction);
