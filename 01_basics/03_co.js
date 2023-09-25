999e1a3424e2f9876f6d47af3189e61c1468e603
ankitgit12 <144496631+ankitgit12@users.noreply.github.com>
2023-09-25 19:53:50 +0000

    conversion of data type

--- 01_basics/03_co.js
+++ 01_basics/03_co.js
@@ -18,3 +18,40 @@ let someNumber = 653
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+ /////////**********operation*******************/\\\\\\\\\
+
+
+
+ let value = 3
+ var negValue = -value
+ console.log(negValue);
+
+
+ console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**2);
+ console.log(2%2);
+
+ let str1 = "ankit"
+
+ let str2 = " hello"
+ let str3 = str1 + str2
+ console.log(str3);
+
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log("1" +2+2);
+ console.log(1+1+"2");
+
+ console.log(3-4*5%3);
+ console.log(+true);
+ console.log(+"");
+ let num1, num2, num3
+ num1 = num2 = num3 =2 + 2
+
+ let gameCounter = 100
+ gameCounter++;
+ console.log(gameCounter);
